b3597f13dcb1bb3d05320ee8c0946a0c5ac46fc2
LuisFer2500 <luisrodriguez2500@gmail.com>
2024-02-16 23:52:55 -0500

    ms-registro

---
+++ src/main/java/com/example/msrodriguezesquivel/MsRodriguezesquivelApplication.java
@@ -0,0 +1,13 @@
+package com.example.msrodriguezesquivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRodriguezesquivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRodriguezesquivelApplication.class, args);
+	}
+
+}
